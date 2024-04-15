b3be5d66f9d146f4558d52ee6ff30adb220ceb0a
Tom Adriaens <tomadriaens@hotmail.com>
2024-04-15 13:45:34 +0200

    Add project files.

---
+++ Array_Oef6_2lagen/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_Oef6_2lagen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
